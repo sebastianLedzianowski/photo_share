cd275289dea9b49f7bae7ad47c7088479ccb5ba4
CamilleusRex <camilleusrex@gmail.com>
2024-03-02 12:55:38 +0100

    added router for search

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import redis.asyncio as redis
 import uvicorn
 from fastapi import FastAPI
 from fastapi_limiter import FastAPILimiter
-from src.routes import users, auth, messages, pictures_oktawian, tags
+from src.routes import users, auth, messages, tags, pictures_oktawian, search
 from src.services.secrets_manager import get_secret
 from fastapi.middleware.cors import CORSMiddleware
 
@@ -22,18 +22,20 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+# Include the routers for the different routes
 app.include_router(auth.router, prefix='/api')
 app.include_router(users.router, prefix='/api')
 app.include_router(messages.router, prefix='/api')
+app.include_router(search.router, prefix='/api')
 app.include_router(pictures_oktawian.router, prefix='/api')
 app.include_router(tags.router, prefix='/api')
 
-
+# Get the Redis connection details from the secrets manager
 REDIS_HOST = get_secret("REDIS_HOST")
 REDIS_PORT = get_secret("REDIS_PORT")
 REDIS_PASSWORD = get_secret("REDIS_PASSWORD")
 
-
+# Initialize FastAPILimiter on application startup
 @app.on_event("startup")
 async def startup():
     """
@@ -48,11 +50,11 @@ async def startup():
     )
     await FastAPILimiter.init(r)
 
-
+# Add a simple ping endpoint
 @app.get("/ping")
 def root():
     return {"message": "pong"}
 
-
+# Run the application using uvicorn
 if __name__ == "__main__":
     uvicorn.run("main:app", reload=True)
