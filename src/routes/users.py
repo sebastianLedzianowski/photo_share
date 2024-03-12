b6b9ed2fa7a02986577663ed3d71a2eb8d25bdaa
bgozlinski <bartlomiej.gozlinski@gmail.com>
2024-03-12 21:16:59 +0100

    new logic

--- src/routes/users.py
+++ src/routes/users.py
@@ -1,16 +1,13 @@
 from typing import List
-
-from fastapi import APIRouter, Depends, UploadFile, File, status, HTTPException
+from fastapi import APIRouter, Depends, UploadFile, File, HTTPException
 from fastapi_limiter.depends import RateLimiter
 from sqlalchemy.orm import Session
 import cloudinary
 import cloudinary.uploader
-
 from src.database.db import get_db
 from src.database.models import User
 from src.repository import users as repository_users
 from src.repository.users import get_user_by_id, list_all_users, update_user_name, ban_user, get_user_by_username
-from src.services.admin import require_admin
 from src.services.auth import auth_service
 from src.schemas import UserDb, UserUpdateName
 from src.conf.cloudinary import configure_cloudinary, generate_random_string
@@ -105,7 +102,7 @@ async def update_user_name_route(user_id: int,
 
 
 @router.post("/ban/{user_id}")
-@require_admin
+# @require_admin
 async def ban_user_route(user_id: int, db: Session = Depends(get_db), current_user: User = Depends(auth_service.get_current_user)):
     """
     Ban a specific user identified by their user ID.
