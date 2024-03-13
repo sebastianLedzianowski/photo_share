a5104a8a8f176887d0ae00b4ac4dd31733150432
sebastianLedzianowski <136706736+sebastianLedzianowski@users.noreply.github.com>
2024-03-13 18:31:28 +0100

    update rating.py

--- src/routes/rating.py
+++ src/routes/rating.py
@@ -1,21 +1,18 @@
-from typing import Any, List
-
 from fastapi import Depends, status, APIRouter
 from sqlalchemy.orm import Session
 
 from src.database.db import get_db
 from src.database.models import User
 from src.repository.rating import add_rating_to_picture, remove_rating_from_picture, get_rating, get_average_of_rating
-from src.schemas import RatingValue
+from src.schemas import Rating, RatingPicture
 from src.services.auth import auth_service
 
 router = APIRouter(prefix="/rating", tags=["rating"])
 
 
-@router.post("/{rating}", status_code=status.HTTP_201_CREATED)
+@router.post("/", status_code=status.HTTP_201_CREATED)
 async def create_rating(
-        picture_id: int,
-        rating: RatingValue,
+        data: Rating,
         current_user: User = Depends(auth_service.get_current_user),
         db: Session = Depends(get_db)
 ):
@@ -26,14 +23,15 @@ async def create_rating(
     - **rating**: The rating assigned to the picture, must be one of the allowed values (1-5).
     """
 
-    return await add_rating_to_picture(picture_id, rating, current_user, db)
+    return await add_rating_to_picture(data.picture_id, data.rating, current_user, db)
 
 
-@router.delete("/{rating}")
+@router.delete("/{picture_id}")
 async def delete_rating(
         picture_id: int,
         current_user: User = Depends(auth_service.get_current_user),
-        db: Session = Depends(get_db)):
+        db: Session = Depends(get_db)
+):
     """
     Removes a user's rating for a picture.
 
@@ -42,26 +40,28 @@ async def delete_rating(
     result = await remove_rating_from_picture(picture_id, current_user, db)
     return result
 
-@router.get("/{picture_id}")
+@router.post("/picture")
 async def get_ratings(
-        picture_id: int,
-        db: Session = Depends(get_db)):
+        data: RatingPicture,
+        db: Session = Depends(get_db)
+):
     """
     Retrieves ratings for a picture.
 
     - **picture_id**: The identifier of the picture whose ratings are to be retrieved.
     """
-    ratings = await get_rating(picture_id, db)
+    ratings = await get_rating(data.picture_id, db)
     return ratings
 
-@router.get("/average/{picture_id}")
+@router.post("/average/picture")
 async def get_average_rating(
-        picture_id: int,
-        db: Session = Depends(get_db)):
+        data: RatingPicture,
+        db: Session = Depends(get_db)
+):
     """
     Calculates the average rating for a picture.
 
     - **picture_id**: The identifier of the picture for which the average rating is to be calculated.
     """
-    average = await get_average_of_rating(picture_id, db)
+    average = await get_average_of_rating(data.picture_id, db)
     return average
